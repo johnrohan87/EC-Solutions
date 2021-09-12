3f5a8c7edfae0b8f62a086cbc5656ca3cb009318
John Rohan <john.rohan87@gmail.com>
2021-09-12 02:00:01 -0400

    added indieauth to head

--- packages/landing-gatsby/src/components/seo.js
+++ packages/landing-gatsby/src/components/seo.js
@@ -63,6 +63,14 @@ function Seo({ description, lang, meta, keywords, title }) {
                 .concat(meta)}
             />
             <Helmet>
+              <link
+                rel="authorization_endpoint"
+                href="https://indieauth.com/auth"
+              />
+              <link
+                rel="token_endpoint"
+                href="https://tokens.indieauth.com/token"
+              />
               <link rel="me" href="https://twitter.com/ECSolutionsGrp" />
               <link
                 rel="webmention"
